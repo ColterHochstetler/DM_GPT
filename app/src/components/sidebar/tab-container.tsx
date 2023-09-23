aa6147e05a83904e0beba49d0eb645ceb5e1a26e
ColterH <colterhawkstetler@gmail.com>
2023-09-23 10:58:40 -0700

    TabContainer now supports an X button to clear tabs (untested)

--- app/src/components/sidebar/tab-container.tsx
+++ app/src/components/sidebar/tab-container.tsx
@@ -1,6 +1,5 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Tabs } from '@mantine/core';
-
 
 type TabProps = {
     name: string;
@@ -12,31 +11,45 @@ type TabProps = {
   type TabContainerProps = {
     tabs: TabProps[];
   };
-  
-  
 
   const TabContainer: React.FC<TabContainerProps> = ({ tabs }) => {
-    const iconStyle = { width: '12px', height: '12px' };
-  
+    const [activeTab, setActiveTab] = useState<string | null>(tabs[0]?.value);
+    const iconStyle = { width: '12px', height: '12px', marginRight: '8px' };
+
+    const handleTabChange = (event: React.FormEvent<HTMLDivElement>) => {
+      const newValue = (event.target as HTMLInputElement).value;
+      setActiveTab(newValue);
+  };
+
     return (
-      <Tabs defaultValue={tabs[0]?.value}>
+      <Tabs value={activeTab} onChange={handleTabChange}>
         <Tabs.List>
           {tabs.map((tab) => (
-            <Tabs.Tab key={tab.value} value={tab.value} leftSection={tab.icon ? React.cloneElement(tab.icon, { style: iconStyle }) : null}>
+            <Tabs.Tab key={tab.value} value={tab.value}>
+              {tab.icon && React.cloneElement(tab.icon, { style: iconStyle })}
               {tab.name}
             </Tabs.Tab>
           ))}
         </Tabs.List>
-  
+
         {tabs.map((tab) => (
-          <Tabs.Panel key={tab.value} value={tab.value} pt="xs">
-            {tab.content}
-          </Tabs.Panel>
+          activeTab === tab.value && (
+            <Tabs.Panel key={tab.value} value={tab.value} pt="xs">
+              <div style={{ position: 'relative' }}>
+                {/* Close Button */}
+                <button 
+                  style={{ position: 'absolute', top: '10px', right: '10px', background: 'none', border: 'none', cursor: 'pointer' }}
+                  onClick={() => setActiveTab(null)}
+                >
+                  X
+                </button>
+                {tab.content}
+              </div>
+            </Tabs.Panel>
+          )
         ))}
       </Tabs>
     );
-  };
-  
- 
+};
 
 export default TabContainer;
